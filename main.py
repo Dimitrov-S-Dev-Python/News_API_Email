e7002ab3dab67a0e5f8f90cd06da893da7050fa0
Stan <dimitrov.s.dev@gmail.com>
2023-04-14 22:42:20 +0300

    Add Links , Specify language, and numbers of news, add subject

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "96db5ae4e191484d98de360414df940c"
-url = "https://newsapi.org/v2/everything?q=tesla&"\
-        "from=2023-03-14&sortBy=publishedAt&apiKey="\
-        "96db5ae4e191484d98de360414df940c"
+url = "https://newsapi.org/v2/everything?"\
+    f"q={topic}&" \
+    "from=2023-03-14&" \
+    "sortBy=publishedAt&" \
+    "apiKey=96db5ae4e191484d98de360414df940c&" \
+    "language=en"
 
 # Make request
 request = requests.get(url)
@@ -14,9 +19,11 @@ content = request.json()
 
 # Access the article titles and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:5]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = "Subject: Today's news" + "\n" + body + article["title"] +\
+               "\n" + article["description"] +\
+               "\n" + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(body)
